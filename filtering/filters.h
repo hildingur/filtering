db2049e17e6c2699fad7d556b40ce3b80ac966bf
hildingur <deineshmahanti@gmail.com>
2013-05-10 08:16:13 -0400

    code cleanup. added comments.

--- filtering/filters.h
+++ filtering/filters.h
@@ -1,12 +1,20 @@
 #ifndef filters_h
 #define filters_h
-// log_stock_prices are the log of stock prices
-// muS is the real-world stock drift
-// n_stock_prices is the number of the above stock prices
-// (omega, theta, xi, rho) are the Heston parameters
-// u[] is the set of means of observation errors
-// v[] is the set of variances of observation errors
-// estimates[] are the estimated observations from the filter
+
+/**
+This file contains the function headers for model parameter estimators 
+as defined in Alireza Javaheri's book, "Inside Volatility Arbitrage"
+**/
+
+/*
+ log_stock_prices are the log of stock prices
+ muS is the real-world stock drift
+ n_stock_prices is the number of the above stock prices
+ (omega, theta, xi, rho) are the Heston parameters
+ u[] is the set of means of observation errors
+ v[] is the set of variances of observation errors
+ estimates[] are the estimated observations from the filter
+*/
 void estimate_extended_kalman_parameters_1_dim(
 	double *log_stock_prices,
 	double muS,
