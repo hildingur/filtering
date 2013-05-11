f25c15915798b7f7e14292287453793cd37869ed
hildingur <deineshmahanti@gmail.com>
2013-05-11 16:46:45 -0400

    upates the R plotter.

--- r_scripts/heston_ekf_plotter.R
+++ r_scripts/heston_ekf_plotter.R
@@ -2,7 +2,12 @@ require(data.table)
 require(reshape)
 require(ggplot2)
 
+#Run install.packages("data.table", "reshape", "ggplot2")
+#if you are missing the above packages.
+
 res <- data.table(read.csv("../io/out.csv"))
 mres <- data.table(melt(res, id.vars='iteration'))
 qplot(iteration, value, data = mres[variable != "likelihood"], geom='step') +
-    facet_wrap(~variable, ncol=2, scales="free_y")
+    facet_wrap(~variable, ncol=2, scales="free_y") +
+  opts(title=expression("Heston parameters for IBM as calculated by EKF"))
+
