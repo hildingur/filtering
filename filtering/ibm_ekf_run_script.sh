7a3700cd2c5f54b8c50996efcfdbe2b7e898bbb8
hildingur <deineshmahanti@gmail.com>
2013-05-11 20:12:19 -0400

    added run scripts for the ekf and ukf

---
+++ filtering/ibm_ekf_run_script.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+
+./bin/heston_ekf "../io/ibm_trimmed.csv" "../io/ibm_heston_ekf_params.csv"
---
+++ filtering/ibm_ukf_run_script.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+
+./bin/heston_ukf "../io/ibm_trimmed.csv" "../io/ibm_heston_ukf_params.csv"
