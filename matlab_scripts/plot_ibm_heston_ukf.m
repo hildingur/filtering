86c18f5bc371c95a51b3ea58108fc1c2693b9980
hildingur <deineshmahanti@gmail.com>
2013-05-11 22:07:52 -0400

    added the grid to the plot.

--- matlab_scripts/plot_ibm_heston_ukf.m
+++ matlab_scripts/plot_ibm_heston_ukf.m
@@ -6,6 +6,7 @@ subplot(2, 2, 1);
 omega = heston(:,2);
 iterations = heston(:,1);
 plot(iterations, omega);
+grid on;
 xlim([0, max(iterations)]);
 xlabel('# iterations');
 ylab = ylabel('\omega', 'rot', 0); %By convention, the second column is omega
@@ -19,6 +20,7 @@ subplot(2, 2, 2);
 theta = heston(:,3);
 iterations = heston(:,1);
 plot(iterations, theta);
+grid on;
 xlim([0, max(iterations)]);
 xlabel('# iterations');
 ylabel('\theta', 'rot', 0); %By convention, the third column is theta
@@ -32,6 +34,7 @@ subplot(2, 2, 3);
 xi = heston(:,4);
 iterations = heston(:,1);
 plot(iterations, xi);
+grid on;
 xlim([0, max(iterations)]);
 xlabel('# iterations');
 ylabel('\xi', 'rot', 0); %By convention, the third column is theta
@@ -44,6 +47,7 @@ subplot(2, 2, 4);
 rho = heston(:,5);
 iterations = heston(:,1);
 plot(iterations, rho);
+grid on;
 xlim([0, max(iterations)]);
 xlabel('# iterations');
 ylabel('\rho', 'rot', 0); %By convention, the third column is theta
